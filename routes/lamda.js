3b427b23d8b045110f8fb6ce30c543be93ef140b
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-12-18 14:48:28 -0800

    lamda

---
+++ routes/lamda.js
@@ -0,0 +1,79 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+"use strict";
+const Alexa = require("alexa-sdk");
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID = "amzn1.ask.skill.aacf2066-fbd6-4006-953c-2b568ef7281a";
+
+const SKILL_NAME = "I Don't Know, You Decide";
+const HELP_MESSAGE =
+  "You can say somthing like, Find a place to eat at, or, Find me a place Burger joint in Seattle to eat at. What can I help you with?";
+const HELP_REPROMPT = "What can I help you with?";
+const STOP_MESSAGE = "Goodbye!";
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
+//=========================================================================================================================================
+const greetings = [
+  "Hey! If you are pretty indecisive, you can ask things like, find me a place to eat, or, find a place to eat in Seattle.",
+  "Hello! If you know they type of food you want to eat, you can ask things like, find me a thai place to eat, or, find a burger place to eat in Seattle.",
+  "Hello! You can leave all the decision making up to us, just ask, find me a place to eat, or find me a place in Bellevue",
+  "Hey! You can be very direct, just ask, find me an ice cream place to eat in Redmond, or to leave it up to us, just say, find me a place to eat."
+];
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+function buildHandlers(event) {
+  handlers = {
+    LaunchRequest: function() {
+      this.emit("GetNewFactIntent");
+    },
+    GetNewFactIntent: function() {
+      const greetingsIndex = Math.floor(Math.random() * greetings.length);
+      const randomGreeting = factArr[greetingsIndex];
+      const speechOutput = GET_FACT_MESSAGE + randomGreeting;
+
+      this.response.cardRenderer(SKILL_NAME, randomGreeting);
+      this.response.speak(speechOutput);
+      this.emit(":responseReady");
+    },
+    "AMAZON.HelpIntent": function() {
+      const speechOutput = HELP_MESSAGE;
+      const reprompt = HELP_REPROMPT;
+
+      this.response.speak(speechOutput).listen(reprompt);
+      this.emit(":responseReady");
+    },
+    "AMAZON.CancelIntent": function() {
+      this.response.speak(STOP_MESSAGE);
+      this.emit(":responseReady");
+    },
+    "AMAZON.StopIntent": function() {
+      this.response.speak(STOP_MESSAGE);
+      this.emit(":responseReady");
+    }
+  };
+}
+
+exports.handler = function(event, context, callback) {
+  const alexa = Alexa.handler(event, context, callback);
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(buildHandlers);
+  alexa.execute();
+};
